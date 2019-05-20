fca2c762bf970f6670ab05a9ef65f32404f959f3
Brayden Allen <allenbrayden@gmail.com>
2019-05-20 01:22:35 -0600

    finished assignment

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: false
+    };
+
+    this.showMenu = this.showMenu.bind(this);
+  }
+
+  showMenu() {
+    this.setState({showMenu: !this.state.showMenu});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav>
+          <div className='container'>
+            <div className='title'>Start Bootstrap</div>
+            <ul className='nav-links'>
+              <li>Services</li>
+              <li>Portfolio</li>
+              <li>About</li>
+              <li>Team</li>
+              <li>Contact</li>
+            </ul>
+            <button className='nav-button' onClick={this.showMenu}>Menu &#9776;</button>
+          </div>
+            <div className={this.state.showMenu ? 'nav-menu nav-menu-show' : 'nav-menu'}>
+              <ul className='nav-list'>
+                <li>Services</li>
+                <li>Portfolio</li>
+                <li>About</li>
+                <li>Team</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+        </nav>
+      <header>
+        <div className='header-container'>
+        <div className='welcome'>Welcome to Our Studio!</div>
+        <div className='meet-you'>It's nice to meet you</div>
+        <button className='contact'>Tell Me More</button>
+        </div>
+      </header>
+      <section>
+      </section>
       </div>
     );
   }
 }
-
 export default App;
